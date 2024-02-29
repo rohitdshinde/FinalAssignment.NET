9ef7d2708df595c0d258ebe3a46f03f8aabd6948
mcs149 <mcs149@MSCCS30>
2024-02-29 11:12:27 +0530

    Add project files.

---
+++ FinalAssignment.NET/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalAssignment.NET
+{
+    internal class Class1
+    {
+        public void Q1()
+        {
+           Console.WriteLine("Enter a string");
+           string strr =Console.ReadLine();
+
+            if (strr.Length >= 2)
+            {
+                char[] arr=strr.ToCharArray();
+                char temp = arr[0];
+                arr[0]= arr[strr.Length - 1];
+                arr[strr.Length-1]= temp;
+
+                string strr1=new string(arr);
+                Console.WriteLine("After swapping "+strr1);
+            }
+            else
+            {
+                Console.WriteLine("plz enter atlast more charcter");
+            }
+        }
+        public void Q2()
+        {
+            Console.WriteLine("Enter a number");
+            int a=Convert.ToInt32(Console.ReadLine());
+
+            int sum = 0, digit, og = a;
+            while(a!=0) {
+                digit = a % 10;
+                sum += digit;
+                a=a/10;
+            }
+            Console.WriteLine($"sum of every number{og}: {sum}");
+
+        }
+        public void Q3()
+        {
+
+        }
+        public void Q4()
+        {
+
+        }
+    }
+}
---
+++ FinalAssignment.NET/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalAssignment.NET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 p= new Class1();
+           // p.Q1();
+            p.Q2();
+           
+        }
+    }
+}
